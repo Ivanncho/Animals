21a8f26a4566bff46cebdef0f5cdebf2957ca142
Ivan <Ivan@TF0551>
2021-02-19 20:37:46 +0100

    Adding first variables and methods in Animals class

--- Animals/src/Animals.java
+++ Animals/src/Animals.java
@@ -1,4 +1,32 @@
 
 public class Animals {
-
+	private String name;
+	private double height;
+	private String color;
+	
+	public void eat() {
+		System.out.println("Eating...");
+	}
+	public void sleep() {
+		System.out.println("Sleeping...ZzZz");
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
 }
