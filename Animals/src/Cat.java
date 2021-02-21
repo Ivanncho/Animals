085953b291c336cc87e036dc9cc2caa9a2d6d1a4
Ivan <Ivan@TF0551>
2021-02-21 08:38:55 +0100

    Adding interface Pets and implementing it with all its methods

--- Animals/src/Cat.java
+++ Animals/src/Cat.java
@@ -1,10 +1,22 @@
 
-public class Cat extends Feline {
+public class Cat extends Feline implements Pets {
 	
 
 	@Override
 	public void makeNoise() {
 		System.out.println("Miiiaauu");
+		
+	}
+
+	@Override
+	public void beFriendly() {
+		System.out.println("Purring around you");
+		
+	}
+
+	@Override
+	public void play() {
+		System.out.println("Pushing the cotton fur ball around");
 		
 	}
 }
--- Animals/src/Dog.java
+++ Animals/src/Dog.java
@@ -1,10 +1,22 @@
 
-public class Dog extends Canine {
+public class Dog extends Canine implements Pets {
 	
 
 	@Override
 	public void makeNoise() {
 		System.out.println("Woof Woof..");
+		
+	}
+
+	@Override
+	public void beFriendly() {
+		System.out.println("Licking your hand");
+		
+	}
+
+	@Override
+	public void play() {
+		System.out.println("Jumping on your leg and running away");
 		
 	}
 }
