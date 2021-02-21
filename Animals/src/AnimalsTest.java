7d215339893c4cd3a502da7ff83f0afa991c6a0f
Ivan <Ivan@TF0551>
2021-02-21 08:44:56 +0100

    Testing Vet class

--- Animals/src/AnimalsTest.java
+++ Animals/src/AnimalsTest.java
@@ -5,6 +5,11 @@ public class AnimalsTest {
 		Dog d = new Dog();
 		d.setName("Fibo");
 		System.out.println(d.getName());
+		System.out.println("*********");
+		Vet v = new Vet();
+		v.giveShot(d);
+		Cat c = new Cat();
+		v.giveShot(c);
 	}
 
 }
--- Animals/src/Vet.java
+++ Animals/src/Vet.java
@@ -1,7 +1,7 @@
 
 public class Vet {
 	public void giveShot(Animals a) {
-		System.out.println("Giving shot");
+		System.out.println("Giving shot to a " + a.getClass());
 		a.makeNoise();
 	}
 }
